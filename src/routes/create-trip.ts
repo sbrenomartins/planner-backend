d2eeadbe83fe1699e2baecbc93eea5b4b8573a97
Breno M. Silva <sbrenomartins@gmail.com>
2024-08-02 20:22:33 -0300

    feat: create routes, schemas and collections for activities and links

--- src/routes/create-trip.ts
+++ src/routes/create-trip.ts
@@ -2,14 +2,9 @@ import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
 import { z } from "zod";
 import { prisma } from "../lib/prisma";
-import dayjs from "dayjs";
-import "dayjs/locale/pt-br";
-import localizedFormat from "dayjs/plugin/localizedFormat";
 import nodemailer from "nodemailer";
 import { getMailClient } from "../lib/mail";
-
-dayjs.locale("pt-br");
-dayjs.extend(localizedFormat);
+import { dayjs } from "../lib/dayjs";
 
 export async function createTrip(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
