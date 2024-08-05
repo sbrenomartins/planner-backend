a828712b6c8504715bf79ce133bd3b723325d59f
Breno M. Silva <sbrenomartins@gmail.com>
2024-08-05 20:37:08 -0300

    feat: finishing project

--- src/routes/update-trip.ts
+++ src/routes/update-trip.ts
@@ -5,24 +5,24 @@ import { prisma } from "../lib/prisma";
 import { dayjs } from "../lib/dayjs";
 import { ClientError } from "../errors/client-error";
 
-export async function createActivity(app: FastifyInstance) {
-  app.withTypeProvider<ZodTypeProvider>().post(
-    "/trips/:tripId/activities",
+export async function updateTrip(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().put(
+    "/trips/:tripId",
     {
       schema: {
         params: z.object({
           tripId: z.string().uuid(),
         }),
         body: z.object({
-          title: z.string().min(4),
-          occurs_at: z.coerce.date(),
+          destination: z.string().min(4),
+          starts_at: z.coerce.date(),
+          ends_at: z.coerce.date(),
         }),
       },
     },
     async (request) => {
-      const { title, occurs_at } = request.body;
-
       const { tripId } = request.params;
+      const { destination, ends_at, starts_at } = request.body;
 
       const trip = await prisma.trip.findUnique({
         where: {
@@ -34,22 +34,28 @@ export async function createActivity(app: FastifyInstance) {
         throw new ClientError("Trip not found.");
       }
 
-      if (
-        dayjs(occurs_at).isBefore(trip.starts_at) ||
-        dayjs(occurs_at).isAfter(trip.ends_at)
-      ) {
-        throw new ClientError("Invalid date for activity.");
+      if (dayjs(starts_at).isBefore(new Date())) {
+        throw new ClientError("Invalid trip start date.");
       }
 
-      const activity = await prisma.activity.create({
+      if (dayjs(ends_at).isBefore(starts_at)) {
+        throw new ClientError("Invalid trip end date.");
+      }
+
+      await prisma.trip.update({
+        where: {
+          id: tripId,
+        },
         data: {
-          title,
-          occurs_at,
-          trip_id: tripId,
+          destination,
+          ends_at,
+          starts_at,
         },
       });
 
-      return { activityId: activity.id };
+      return {
+        tripId: trip.id,
+      };
     }
   );
 }
--- src/server.ts
+++ src/server.ts
@@ -11,8 +11,17 @@ import { createActivity } from "./routes/create-activity";
 import { getActivities } from "./routes/get-activities";
 import { createLink } from "./routes/create-link";
 import { getLinks } from "./routes/get-links";
+import { getParticipants } from "./routes/get-participants";
+import { createInvite } from "./routes/create-invite";
+import { updateTrip } from "./routes/update-trip";
+import { getTripDetails } from "./routes/get-trip-details";
+import { getParticipant } from "./routes/get-participant";
+import { errorHandler } from "./errorHandler";
+import { env } from "./env";
 
 const app = fastify();
+
+app.setErrorHandler(errorHandler);
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
@@ -28,7 +37,12 @@ app.register(createActivity);
 app.register(getActivities);
 app.register(createLink);
 app.register(getLinks);
+app.register(getParticipants);
+app.register(createInvite);
+app.register(updateTrip);
+app.register(getTripDetails);
+app.register(getParticipant);
 
-app.listen({ port: 3333 }).then(() => {
+app.listen({ port: env.PORT }).then(() => {
   console.log("Server running!");
 });
